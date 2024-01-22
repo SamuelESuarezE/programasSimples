488d9be38cdb0da2eb307ee65e91afd623890486
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-01-22 12:31:46 -0500

    Promedio correccion y Pseudocodigo

---
+++ 3.Promedio.psc
@@ -0,0 +1,9 @@
+Algoritmo Promedio
+	Para x<-1 Hasta 4 Hacer
+		Escribir 'Ingrese un numero: '
+		Leer new_num
+		suma <- suma+new_num
+	FinPara
+	media <- suma/4
+	Escribir 'El promedio es ', media
+FinAlgoritmo
--- 3.Promedio.py
+++ 3.Promedio.py
@@ -1,11 +1,10 @@
-from statistics import mean
-
 # Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
-num_list=[]
+suma = 0
 
 for x in range(4):
     new_num = float(input("Ingrese un numero: "))
-    num_list.append(new_num)
-    
-print(f"El promedio es {round(mean(num_list), 2)}")
+    suma+=new_num
+promedio=suma/4
+
+print(f"El promedio es {round(promedio, 2)}")
 
